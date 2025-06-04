07b43d3d6400f95acfcfdc98059b20e1934ca4e5
user <user@NOTER023.tkh-staff.local>
2025-06-04 09:20:44 +0900

    Read COBOL in

--- main.py
+++ main.py
@@ -5,15 +5,16 @@
 import os
 from bs4 import BeautifulSoup
 import numpy as np
+import pandas as pd
 
 # Testing Only
 from time import sleep
 
 # Declare Variables
 cpath = os.getcwd()
-out_file = os.path.join(cpath, "cobol_file.py")
 pos_ans = 'YES'
 cobol_text = ''
+cobol_commands = pd.DataFrame()
 
 # Define Functions
 
@@ -24,22 +25,44 @@ def checkUser(user_answer):
     else:
         return False
 
-def getFileFromUser():
+def getFileFromUser(): # Not used as for convience. Can be turned on at the user's discression. 
     file_name = input("What is the name of your cobol file?: ")
     if file_name[-3:] == 'cob':
         return file_name
     else:
         return (file_name + '.cob')
+
+def findCobFile(): # Used to make testing fast. Can be toggled on and off.
+    global cpath
+    for root, dirs, files in os.walk(cpath):
+        for i in files:
+            if i[-3:] == "cob":
+                return os.path.join(cpath, i)
     
 def read_cobol_file(in_file):
     global cobol_text
     with open(in_file) as f:
         cobol_text = f.read()
 
+def parseCobol():
+    global cobol_text
+    cobol_list = cobol_text.split('.')
+    return cobol_list
+
+def getProgramName():
+    global cobol_commands
+
+
 # Execute Program
 
 if __name__ == "__main__":
-    in_file = getFileFromUser()
+    in_file = findCobFile() # Optional. Can be toggled with the line below. 
+    # in_file = getFileFromUser() # Optional. Turned off for testing.
     read_cobol_file(in_file=in_file)
-    print(cobol_text)
-    sleep(25) # Testing only. Remove later.
+    cobol_list = parseCobol()
+    out_name = in_file[:-4]
+    out_file_name = "{}.py".format(out_name) # Change to program_id
+    out_file = os.path.join(cpath, out_file_name)
+    with open(out_file, "w") as f:
+        for i in cobol_list:
+            f.write(i)
